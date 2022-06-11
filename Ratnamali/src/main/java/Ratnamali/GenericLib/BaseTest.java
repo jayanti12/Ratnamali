55a0d4b4d190fc52cfbd6d65904f085dbcb5a2f8
jayanti12 <104966257+jayanti12@users.noreply.github.com>
2022-06-11 12:52:43 +0530

    Update BaseTest.java

--- Ratnamali/src/main/java/Ratnamali/GenericLib/BaseTest.java
+++ Ratnamali/src/main/java/Ratnamali/GenericLib/BaseTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-//return in global report
+//return in global report(Github)
 public class BaseTest implements IAutoConsts{
 
 	public static WebDriver driver;
